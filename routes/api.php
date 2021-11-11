d9ad5f8c4651e894783df098d418ea7f0d7868f1
gabri <gabriel-gadotti@hotmail.com>
2021-11-11 16:53:11 -0300

    App_ngbank_1.0 - 333 a 336 - autorização

--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,14 @@ Route::prefix('v1')->middleware('jwt.auth')->group(function() {
     Route::apiResource('equipe','App\Http\Controllers\EquipeController')->middleware('jwt.auth'); // rota para equipes
     Route::apiResource('funcionario','App\Http\Controllers\FuncionarioController'); // rota para funcionarios
     Route::apiResource('venda','App\Http\Controllers\VendaController');//rota para vendas
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
 });
 // fim rotas do app ____________________________________________________________________________________________________
 
 
 Route::post('login', 'App\Http\Controllers\AuthController@login');
-Route::post('logout', 'App\Http\Controllers\AuthController@logout');
-Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
-Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+
+
