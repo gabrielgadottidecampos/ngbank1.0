a11487e3fbb6d560431d068a5d1024130a83bd0c
gabri <gabriel-gadotti@hotmail.com>
2021-11-11 16:39:35 -0300

    App_ngbank_1.0 - 332 - prefix de rotas

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 // rotas do app ********************************************************************************************************
-Route::apiResource('equipe','App\Http\Controllers\EquipeController'); // rota para equipes
-Route::apiResource('funcionario','App\Http\Controllers\FuncionarioController'); // rota para funcionarios
-Route::apiResource('venda','App\Http\Controllers\VendaController');//rota para vendas
+Route::prefix('v1')->middleware('jwt.auth')->group(function() {
+    Route::apiResource('equipe','App\Http\Controllers\EquipeController')->middleware('jwt.auth'); // rota para equipes
+    Route::apiResource('funcionario','App\Http\Controllers\FuncionarioController'); // rota para funcionarios
+    Route::apiResource('venda','App\Http\Controllers\VendaController');//rota para vendas
+});
 // fim rotas do app ____________________________________________________________________________________________________
+
 
 Route::post('login', 'App\Http\Controllers\AuthController@login');
 Route::post('logout', 'App\Http\Controllers\AuthController@logout');
